122c895a595fb2bd16ed796232d8ff1ae23cd394
LaJay2626 <87913402+LaJay2626@users.noreply.github.com>
2021-08-07 15:01:02 -0400

    uploaded photos

--- javascript/home.js
+++ javascript/home.js
@@ -1,12 +1,29 @@
 
 document.getElementById("button-1").onclick = function () {
-  location.href = "./pages/about.html";
+  document.getElementById("janet").style.display="";
+  document.getElementById("kamala").style.display="none"; 
+  document.getElementById("michelle").style.display="none"; 
+  document.getElementById("lajay").style.display="none";
 };
 
 document.getElementById("button-2").onclick = function () {
-  alert("Hello world!");
+  document.getElementById("janet").style.display="none";
+  document.getElementById("kamala").style.display=""; 
+  document.getElementById("michelle").style.display="none"; 
+  document.getElementById("lajay").style.display="none";
 };
 
 document.getElementById("button-3").onclick = function () {
-  document.getElementById("content").style.color = "blue";
+  document.getElementById("janet").style.display="none";
+  document.getElementById("kamala").style.display="none"; 
+  document.getElementById("michelle").style.display=""; 
+  document.getElementById("lajay").style.display="none";
+};
+
+
+document.getElementById("button-4").onclick = function () {
+  document.getElementById("janet").style.display="none";
+  document.getElementById("kamala").style.display="none"; 
+  document.getElementById("michelle").style.display="none"; 
+  document.getElementById("lajay").style.display="";
 };
